6ebc48ef1480fe8b33dfdf6fbcfb43b63b77278a
Luizmcla <luizmcla@gmail.com>
2022-11-23 10:08:47 -0300

    first commit

---
+++ api/src/index.ts
@@ -0,0 +1,34 @@
+import path from 'node:path';
+import http from 'node:http';
+import exprees from 'express';
+import mongoose from 'mongoose';
+import { Server } from 'socket.io';
+
+import { router } from './router';
+
+const  app = exprees();
+const server = http.createServer(app);
+export const io = new Server(server);
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const port = 3001;
+
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
+    app.use('/uploads', exprees.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(exprees.json());
+    app.use(router);
+
+    server.listen(port, () => {
+      console.log(`🐱‍👓 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('erro ao conectar no mongo db'));
+
+
